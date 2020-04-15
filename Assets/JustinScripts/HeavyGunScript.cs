bc2acbbfe92291547f267631464d9f6e6b8d768f
Hollowfires <hollowfires@hotmail.com>
2020-04-14 21:13:21 -0700

    hopefully This Works

---
+++ Assets/JustinScripts/HeavyGunScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeavyGunScript : MonoBehaviour
+{
+    public float chargeTime;
+    public float bulletSpeed;
+    public GameObject bullet;
+    public int ammo;
+    public bool infiniteAmmo;
+
+    private float timer = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetButton("Fire1") && transform.parent.name == "Player1Weapon") || 
+        (Input.GetButton("Fire2") && transform.parent.name == "Player2Weapon"))
+        {
+            if (timer < chargeTime)
+            {
+                timer += Time.deltaTime;
+                //Debug.Log("Charging");
+            }
+            else
+            {
+                //Debug.Log("done");
+            }
+        }
+        else
+        {
+            if (timer < chargeTime && timer > 0.0f)
+            {
+                timer -= Time.deltaTime;
+            }
+            if (timer >= chargeTime && ammo > 0)
+            {
+                if (!infiniteAmmo)
+                {
+                    ammo--;
+                }
+                timer = 0.0f;
+                var projectile = Instantiate(bullet);
+                projectile.transform.position = transform.position;
+                projectile.GetComponent<Rigidbody2D>().velocity = (projectile.transform.position - transform.parent.parent.position) * bulletSpeed;
+            }
+        }
+    }
+}
---
+++ Assets/JustinScripts/Player2Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Movement : MonoBehaviour
+{
+    //speed for player
+    public float speed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Camera cam;
+
+    //used to store position
+    Vector2 movement;
+    Vector2 mousePosition;
+    Vector2 aim;
+    float playerAngle;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        movement.x = Input.GetAxisRaw("Horizontal2");
+        movement.y = Input.GetAxisRaw("Vertical2");
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        aim = mousePosition - rb.position;
+        playerAngle = Mathf.Atan2(aim.y, aim.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = playerAngle;
+    }
+
+}
