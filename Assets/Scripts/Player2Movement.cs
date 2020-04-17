6a3daddb63060afbbaccec1b7b0150b6ebb5b8be
Hollowfires <hollowfires@hotmail.com>
2020-04-17 14:21:54 -0700

    playerScripts

---
+++ Assets/Scripts/Player2Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Movement : MonoBehaviour
+{
+    //speed for player
+    public float speed = 5f;
+
+    public Rigidbody2D rb;
+
+    //used to store position
+    Vector2 movement;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        movement.x = Input.GetAxisRaw("Horizontal2");
+        movement.y = Input.GetAxisRaw("Vertical2");
+
+       
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+    
+}
