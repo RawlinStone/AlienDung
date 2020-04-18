5d1b752467cc8641a5df042f82eac7e7a9d23139
IvanAKim <Ivanalejandrokim@gmail.com>
2020-04-17 21:49:31 -0700

    Added Player Movement Animation

--- Assets/JustinScripts/PlayerMovement.cs
+++ Assets/JustinScripts/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
 
     public Rigidbody2D rb;
+    public Animator animator;
+    public float px;
+    public float py;
    
 
     //used to store position
@@ -27,6 +30,42 @@ public class PlayerMovement : MonoBehaviour
         {
             movement.x = Input.GetAxisRaw("Horizontal");
             movement.y = Input.GetAxisRaw("Vertical");
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("speed", movement.sqrMagnitude);
+            if(px > movement.x)
+            {
+                animator.SetBool("right",true);
+                animator.SetBool("left", false);
+                animator.SetBool("up", false);
+                animator.SetBool("down", false);
+            }
+
+            if (px < movement.x)
+            {
+                animator.SetBool("right", false);
+                animator.SetBool("left", true);
+                animator.SetBool("up", false);
+                animator.SetBool("down", false);
+            }
+
+            if(py < movement.y)
+            {
+                animator.SetBool("right", false);
+                animator.SetBool("left", false);
+                animator.SetBool("up", false);
+                animator.SetBool("down", true);
+            }
+
+            if (py > movement.y)
+            {
+                animator.SetBool("right", false);
+                animator.SetBool("left", false);
+                animator.SetBool("up", true);
+                animator.SetBool("down", false);
+            }
+            px = movement.x;
+            py = movement.y;
 
         }
         if (transform.parent.name == "Player2")
@@ -42,4 +81,6 @@ public class PlayerMovement : MonoBehaviour
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
         
     }
+
+    
 }
