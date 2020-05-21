3657c2bfdd871831cbc4309931ec70a7944a7b0f
IvanAKim <Ivanalejandrokim@gmail.com>
2020-05-21 16:59:09 -0700

    RoomChangeMiniUpdate

--- Assets/Scripts/RoomChange.cs
+++ Assets/Scripts/RoomChange.cs
@@ -4,88 +4,78 @@ using UnityEngine;
 
 public class RoomChange : MonoBehaviour
 {
-    public GameObject player1Position;
-    public GameObject player2Position;
-    public Vector2 changeCameraTop;
-    public Vector2 changeCameraBottom;
-    public Vector3 playerChange;
-    public Vector3 optionalplayerChange;
-    public Camera MainCamera;
-    public float cameraSize;
-    private CameraMovement cam;
-    public bool changeOccured;
+    public Transform player1pos;
+    public Transform player2pos;
+    public Camera cam;
+    public float camSize;
+    public Vector3 roomTransform;
+    public Vector3 playerMove;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        cam = Camera.main.GetComponent<CameraMovement>();
-        MainCamera = Camera.main;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        
+
         Debug.Log(collision.gameObject.tag);
         if (collision.CompareTag("Player"))
         {
-            Vector2 temp1 = cam.maxCam;
-            Vector2 temp2 = cam.minCam;
-            float temp3 = MainCamera.orthographicSize;
-            cam.maxCam = changeCameraTop;
-            cam.minCam = changeCameraBottom;
-            changeCameraTop = temp1;
-            changeCameraBottom = temp2;
-            MainCamera.orthographicSize = cameraSize;
-            cameraSize = temp3;
-            if (changeOccured)
+            changeCameraSize();
+            changeCameraPosition();
+            if(collision.gameObject.name == "Player1Sprite")
             {
-                
-                collision.gameObject.transform.position -= optionalplayerChange;
-                Debug.Log(collision.gameObject.name); 
-                if(collision.gameObject.name == "Player1Sprite")
-                {
-                    player2Position.transform.position = collision.gameObject.transform.position;
-                }
-                else if (collision.gameObject.name == "Player2Sprite")
-                {
-                    player1Position.transform.position = collision.gameObject.transform.position;
-                }
-                changeOccured = false;
+                collision.gameObject.transform.position += playerMove;
+                player2pos.transform.position = collision.gameObject.transform.position;
+                playerMove = -playerMove;
             }
-            else
+            else if (collision.gameObject.name == "Player2Sprite")
             {
-                collision.gameObject.transform.position += playerChange;
-                Debug.Log(collision.gameObject.name);
-                if (collision.gameObject.name == "Player1Sprite")
-                {
-                    player2Position.transform.position = collision.gameObject.transform.position;
-                }
-                else if(collision.gameObject.name == "Player2Sprite")
-                {
-                    player1Position.transform.position = collision.gameObject.transform.position;
-                }
-                 
-              
-                changeOccured = true;
+                collision.gameObject.transform.position += playerMove;
+                player1pos.transform.position = collision.gameObject.transform.position;
+                playerMove = -playerMove;
             }
 
-           
+
+
+
+
+
         }
     }
 
-    
+    private void changeCameraSize()
+    {
+        float tempSize = cam.orthographicSize;
+        cam.orthographicSize = camSize;
+        camSize = tempSize;
+    }
 
-    
+    private void changeCameraPosition()
+    {
+        Vector3 temp = cam.transform.position;
+        cam.transform.position = roomTransform;
+        roomTransform = temp;
+
+    }
 
 
 
-    
+
+
+
+
+
 
 }
