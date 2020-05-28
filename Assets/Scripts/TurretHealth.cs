9e2bf4d124f04322ed5d6fe6d7ffcdcf966241f0
IvanAKim <Ivanalejandrokim@gmail.com>
2020-05-28 16:44:43 -0700

    smallTurret

---
+++ Assets/Scripts/TurretHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int health;
+    void Start()
+    {
+        health = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
