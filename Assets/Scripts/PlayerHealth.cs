80833bed1695ce081327837585e29b92c49073c7
IvanAKim <Ivanalejandrokim@gmail.com>
2020-05-27 18:01:08 -0700

    PlayerBlinksWhenTakingDamage

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public int health;
+    public SpriteRenderer renderer;
+    public bool hurt;
+    public float hurtTime;
+    private float hurtCounter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 100;
+        hurt = false;
+        renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -16,13 +22,40 @@ public class PlayerHealth : MonoBehaviour
     {
         if(health <= 0)
         {
-            //destroy player
+            this.gameObject.SetActive(false);
         }
+        if(hurt)
+        {
+            if(hurtCounter > hurtTime * .66f)
+            {
+                renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, 0f);
+            }
+            else if(hurtCounter > hurtTime * .33f)
+            {
+                renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, 1f);
+            }
+            else if(hurtCounter > 0f)
+            {
+                renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, 0f);
+            }
+
+            else
+            {
+                renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, 1f);
+                hurt = false;
+            }
+            hurtCounter -= Time.deltaTime;
+        }
+       
     }
 
     public void PlayerTakeDamage(int damage)
     {
-        health -= damage; 
+        health -= damage;
+        hurt = true;
+        hurtCounter = hurtTime;
+        
     }
 
+    
 }
