ae796e9ce189be5fdf6a8e78b48c420532f086e0
rigel kuper <rkuper@uci.edu>
2020-05-29 16:51:44 -0700

    Enemy drops objects: if any player is below 100 hp then drop either health, ammo, or nothing; otherwise drop ammo or nothing

---
+++ Assets/RigelScripts/PickUpItems.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItems : MonoBehaviour
+{
+    public enum pickUpObject{AMMO, Health};
+    public pickUpObject currentObject;
+    //TODO: Pick up health and/or ammo
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (currentObject == pickUpObject.AMMO)
+            {
+                collision.GetComponent<PlayerAmmoScript>().changeAmmo(RandomQuantity());
+            }
+            else if (currentObject == pickUpObject.Health)
+            {
+                //call health function
+            }
+            Destroy(this.gameObject);
+        }
+    }
+
+    public int RandomQuantity()
+    {
+        //random value between 1-5
+        int count = Random.Range(1,5);
+        Debug.Log(count);
+        return count;
+    }
+}
