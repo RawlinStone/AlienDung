302c968d546149e3d5e3f7462708655f28c60d67
Zongde Liu <zongdel@uci.edu>
2020-06-10 15:56:59 -0700

    Added MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject creditsPanel;
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void showCredits()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void closeCredits()
+    {
+        creditsPanel.SetActive(false);
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
